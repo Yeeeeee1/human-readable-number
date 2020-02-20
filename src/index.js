f7cb828c350e265c70aaf037c93b001a66883b5c
Yeeeeee1 <52548740+Yeeeeee1@users.noreply.github.com>
2020-02-20 23:00:15 +0300

    Complete the human readable numbers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function toReadable (number) {
-  
+	var ones = ['','one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
+	var tens = [ '', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety' ];
+	if (number == 0) return 'zero';
+	if (number < 20) {
+		return ones[number];
+	}
+	var splitted = number.toString().split('');
+	if (number >= 20 && number < 100) {
+        return `${tens[splitted[0]]} ${ones[splitted[1]]}`.trim();
+    }
+    if (number >= 100) {
+        var str1 = ones[splitted[0]] + ' hundred';
+		var remainer = parseInt(splitted[1] + splitted[2], 10);
+		var splittedRemainer = remainer.toString().split('');
+		var str2 = parseInt(splitted[1] + splitted[2]) < 20 ? ones[remainer] : tens[splittedRemainer[0]] + ' ' + ones[splittedRemainer[1]];
+		return `${str1} ${str2}`.trim();
+    }
 }
